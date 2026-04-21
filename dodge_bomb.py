207c04502505d958de7abb89942ba44af0e71e27
tomokiakiyama <qiushantomoki257@gmail.com>
2026-04-21 14:21:50 +0900

    練習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(10, WIDTH - 10), random.randint(10, HEIGHT - 10)
+    vx, vy = 5, 5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -34,7 +41,9 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        bb_rct.move_ip(vx, vy)
         kk_rct.move_ip(sum_mv)
+        screen.blit(bb_img, bb_rct)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
